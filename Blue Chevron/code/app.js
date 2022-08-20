6d47b65eb4c17dc5253532702666e724e408815c
Dr Joseph Jorgen <dr.jojo.virtual@gmail.com>
2022-08-20 04:40:44 -0400

    Added Settings

--- Blue Chevron/code/app.js
+++ Blue Chevron/code/app.js
@@ -6,19 +6,44 @@ var settings = JSON.parse('{' + $("settings")[0].innerHTML
     .map(x => x.trim().replace(/(.+)=(.+)/, '"$1":"$2"'))
     .join(",") + '}');
 
-ComfyJS.Init(settings.USERNAME);
-
-if (settings.DISPLAYORDER == "bottom") {
-
+//common code
+function tryParseInt(int, def) {
+    let parsed = parseInt(int);
+    if (isNaN(parsed)) return def;
+    return parsed;
 }
+
+ComfyJS.Init(settings.USERNAME);
+var opacity = tryParseInt(settings.OPACITY, 100) / 100;
+
+var displayBottom = settings.NEW_MESSAGE_POSITION.toLowerCase() == "bottom";
+if (displayBottom)
+    $("#chat").css("bottom", 0);
+
+var removeMessagesTime = tryParseInt(settings.REMOVE_MESSAGE_AFTER_TIME, 0);
 
 ComfyJS.onChat = (user, message, flags, self, extra) => {
     var dom = $($("#message-template")[0].content.childNodes[1]).clone();
     $(".message-container", dom).attr('id', extra.id);
     $(".user", dom).text(user);
     $(".message", dom).text(message);
+    if (displayBottom) {
+        $("#chat").append($(dom));
+    } else {
+        $("#chat").prepend($(dom));
+    }
+
+    if (removeMessagesTime != 0) {
+        setTimeout(function() {
+            $(dom).css("transition", "visibility 0s 2s, opacity 1s linear");
+            $(dom).css("opacity", 0);
+            $(dom).css("visibility", "hidden");
+        }, removeMessagesTime * 1000);
+        setTimeout(function() {
+            $(dom).remove();
+        }, (2 + removeMessagesTime) * 1000);
+    }
     
-    $("#chat").append($(dom));
 
     var mbSides = $(".message-box .message-left, .message-box .message-right", dom);
     var message = $(".message-box .message", dom);
@@ -28,8 +53,6 @@ ComfyJS.onChat = (user, message, flags, self, extra) => {
     
     $(".message-box", dom).height(message[0].offsetHeight);
     $(dom).height(message[0].offsetHeight - 90 + 132);
-    //message[0].offsetHeight - 90 + 132
-
 
     mbSides.css("border-top", `${h1}px solid transparent`);
     mbSides.css("border-bottom", `${h2}px solid transparent`);
@@ -42,4 +65,13 @@ ComfyJS.onChat = (user, message, flags, self, extra) => {
         .css("transform", `skew(${-deg}deg)`);
     $(".chevron-top.chevron-right, .chevron-bottom.chevron-left", dom)
         .css("transform", `skew(${deg}deg)`);
+
+    if (opacity != 1) {
+        var color = `rgba(24, 24, 24, ${opacity})`;
+        $(".message-box .message, .user-box .buffer", dom).css("background-color", color);
+        $(".user-box .left, .message-box .message-left", dom).css("border-right-color", color);
+        $(".user-box .right, .message-box .message-right", dom).css("border-left-color", color);
+        $(".user-box .user", dom).css("border-top-color", color);
+        $(".user-box .user", dom).css("border-bottom-color", color);
+    }
 }
